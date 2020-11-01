c1b37a22529d04297db5cfb151157eae42daafe0
Tim Shaker <tim@tmshkr.com>
2020-11-01 12:26:51 -0800

    refactor

--- middleware/verifyJWT.js
+++ middleware/verifyJWT.js
@@ -18,23 +18,18 @@ function getPublicKey(header, callback) {
 function verifyJWT(req, res, next) {
   try {
     const token = req.headers.authorization.match(/Bearer (.+)/)[1];
-    const decodedJwt = jwt.decode(token, { complete: true });
-    const { token_use } = decodedJwt.payload;
-
-    if (token_use !== "access")
-      throw new Error("Please provide an access token");
-
     const options = {
       algorithms: ["RS256"],
       issuer: process.env.COGNITO_ISSUER,
     };
 
     jwt.verify(token, getPublicKey, options, function (err, verifiedToken) {
-      if (err) {
+      if (err || verifiedToken.token_use !== "access") {
         console.error(err);
         next({ code: 401, message: "Unauthorized" });
         return;
       }
+
       req.jwt = verifiedToken;
       console.log(req.jwt);
       next();
